817b12761ad58d6fcec2e7a5e09b96928d539cf7
Ryan Howard <rollercodester@live.com>
2024-04-11 12:21:30 -0500

    Add point about startsWithMatching

--- src/views/runtime-config/tab-panels/options.tsx
+++ src/views/runtime-config/tab-panels/options.tsx
@@ -12,9 +12,9 @@ export function OptionsTabPanel() {
       ctaIcon={<IoMdOptions fontSize='1.75rem' />}
       headingContent={
         <VStack sx={{ alignItems: 'flex-start', gap: '1rem' }}>
-          <Text>The Runtime Config is so straightforward that there are only two options to discuss:</Text>
+          <Text>The Runtime Config is so straightforward that there are only three options to discuss:</Text>
           <Box sx={{ fontWeight: 'normal', gap: '2rem', ml: '1rem' }}>
-            <FeatureList features={['Localhost IP Address', 'Manual Active Hostname']} />
+            <FeatureList features={['Localhost IP Address', 'Manual Active Hostname', 'Starts-with Matching']} />
           </Box>
           <Text sx={{ fontWeight: 'normal' }}>
             The <Code>localhostIpAddress</Code> option allows you to set the IP Address that your machine uses for your
@@ -29,6 +29,15 @@ export function OptionsTabPanel() {
             some devops work, so this option makes the most practical sense if you need to use the config in parallel
             across both browser and non-browser environments.
           </Text>
+          <Text sx={{ fontWeight: 'normal' }}>
+            The <Code>startsWithMatching</Code> option allows you to match environments using the hostname and the URL
+            path (instead of just the hostname). The use case for this does not come up often, but it is useful when you
+            have multiple environments that share the same hostname but distinguish the environment via the path. For
+            example, perhaps you have not yet setup multiple infra environments for your frontend, but you want to test
+            different backend environments. In this scenario, you could just add environment paths to your frontend
+            router to emulate different environments (e.g. <Code>myapp.com/dev/</Code> vs <Code>myapp.com/test/</Code>,
+            etc.) and then use those as your config-set keys for your various config settings.
+          </Text>
         </VStack>
       }
       initialOutputMessage='Click the "Run Options Demo" button to execute the demo...'
@@ -37,6 +46,7 @@ export function OptionsTabPanel() {
         const myAppRuntimeConfig = RuntimeConfig.initialize<MyAppConfigSettings, MyAppEnvironments>(myAppConfigSet, {
           localhostIpAddress: '127.0.1.1',
           manualActiveHostname: 'myapp.com',
+          startsWithMatching: false,
         });
 
         logLabel('\nThe runtime config object we just created with options:');
@@ -60,6 +70,7 @@ export const myAppRuntimeConfig = RuntimeConfig.initialize(
   {
     localhostIpAddress: '127.0.1.1',
     manualActiveHostname: 'myapp.com',
+    startsWithMatching: false,
   }
 );
 
